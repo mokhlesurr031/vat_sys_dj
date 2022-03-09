43a08c1ef792673638d5eaa226ac2a96217af855
M. Mahin <mokhlesurr031@gmail.com>
2022-03-10 01:47:47 +0600

    index app added

--- master/master/settings.py
+++ master/master/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'purchase',
     'sales',
     'rest_framework',
+    'index',
 ]
 
 MIDDLEWARE = [
--- master/master/urls.py
+++ master/master/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/', include('products.urls')),
     path('parties/', include('parties.urls')),
+    path('', include('index.urls')),
 ]
