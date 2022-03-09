228b20d5a6e631e0c24e126cc1c42dbb1c849654
M. Mahin <mokhlesurr031@gmail.com>
2022-03-10 00:47:31 +0600

    config for deployment on heroku ready, debug false and whitenoise middleware added

--- master/master/settings.py
+++ master/master/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q&39$u*870_59uyyth5xkzi$n4y0n=24@1ap^jl$qr+g+7+xcc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
